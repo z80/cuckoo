d6ee33819e8e6d978c913f979d223837ae729257
me <me@me.me>
2025-11-30 15:03:03 -0800

    Adjusted dead zones.

--- python/imu00/combined.py
+++ python/imu00/combined.py
@@ -4,10 +4,30 @@ from pyb import Pin, DAC, ADC
 import bmi08
 GAIN_X = 1.0
 GAIN_Y = 1.0
+GAIN_Z = -1.0
 
-OUT_EN_TH = 300
-OUT_DIS_TH = 600
+OUT_EN_TH = 750
+OUT_DIS_TH = 800
 out_en = False
+
+DAC_ZERO = 2048 + 700
+DAC_RANGE = 1340
+
+GYRO_X_DEAD_ZONE = 10
+GYRO_Y_DEAD_ZONE = 10
+GYRO_Z_DEAD_ZONE = 10
+
+# Vertical
+DAC_X_DEAD_ZONE_P = 600 # Down
+DAC_X_DEAD_ZONE_N = 250 # Up
+
+# Not used
+DAC_Y_DEAD_ZONE_P = 600
+DAC_Y_DEAD_ZONE_N = 300
+
+# Horizontal
+DAC_Z_DEAD_ZONE_P = 800 # Left
+DAC_Z_DEAD_ZONE_N = 300 # Right
 
 print( "Entered" )
 # Configure I2C2 with PB10 (SCL) and PB11 (SDA) at 10 kHz
@@ -29,11 +49,13 @@ print( "Initialized FIFO" )
 led1 = pyb.Pin('A15', Pin.OUT)
 led2 = pyb.Pin('C10', Pin.OUT)
 
-dac_x = DAC(Pin('A4'), bits=12)
-dac_y = DAC(Pin('A5'), bits=12)
+dac_x = DAC(Pin('A5'), bits=12)
+dac_y = DAC(Pin('A4'), bits=12)
 
 adc_en = ADC( Pin('A2') )
 pin_en = Pin('A3', Pin.OUT)
+adc_alpha = 0.3
+adc_accum = 600.0
 
 led1.off()
 led2.off()
@@ -46,17 +68,18 @@ while True:
 
     led1.on()
     adc = adc_en.read()
+    adc_accum = adc_alpha*adc + (1.0-adc_alpha)*adc_accum
     if out_en:
-        should_disable = adc > OUT_DIS_TH
+        should_disable = adc_accum > OUT_DIS_TH
         if should_disable:
             out_en = False
-            pin_en.on()
+            pin_en.off()
 
     else:
-        should_enable = adc < OUT_EN_TH
+        should_enable = adc_accum < OUT_EN_TH
         if should_enable:
             out_en = True
-            pin_en.off()
+            pin_en.on()
 
     try:
         x, y, z, qty = imu.read_gyro_sum()
@@ -73,11 +96,42 @@ while True:
         x *= scale
         y *= scale
         z *= scale
-        val_x = int(GAIN_X * z + 2048)
-        val_y = int(GAIN_Y * y + 2048)
-        val_z = int(GAIN_Y * z + 2048)
-        dac_x.write( val_x )
-        dac_y.write( val_y )
+        val_x = int(GAIN_X * x)
+        if val_x > GYRO_X_DEAD_ZONE:
+            val_x += DAC_X_DEAD_ZONE_P
+        elif val_x < -GYRO_X_DEAD_ZONE:
+            val_x -= DAC_X_DEAD_ZONE_N
+        if val_x > DAC_RANGE:
+            val_x = DAC_RANGE
+        elif val_x < -DAC_RANGE:
+            val_x = -DAC_RANGE
+
+        val_y = int(GAIN_Y * y)
+        if val_y > GYRO_Y_DEAD_ZONE:
+            val_y += DAC_Y_DEAD_ZONE_P
+        elif val_y < -GYRO_Y_DEAD_ZONE:
+            val_y -= DAC_Y_DEAD_ZONE_N
+        if val_y > DAC_RANGE:
+            val_y = DAC_RANGE
+        elif val_y < -DAC_RANGE:
+            val_y = -DAC_RANGE
+
+        val_z = int(GAIN_Z * z)
+        if val_z > GYRO_Z_DEAD_ZONE:
+            val_z += DAC_Z_DEAD_ZONE_P
+        elif val_z < -GYRO_Z_DEAD_ZONE:
+            val_z -= DAC_Z_DEAD_ZONE_N
+        if val_z > DAC_RANGE:
+            val_z = DAC_RANGE
+        elif val_z < -DAC_RANGE:
+            val_z = -DAC_RANGE
+
+        out_x = val_x + DAC_ZERO
+        out_y = val_y + DAC_ZERO
+        out_z = val_z + DAC_ZERO
+
+        dac_x.write( out_z )
+        dac_y.write( out_x )
         led2.off()
 
     led1.off()
@@ -86,7 +140,8 @@ while True:
 
     print_counter += 1
     if print_counter >= print_timeout:
-        print( "x: ", val_x, "y: ", val_y, "z: ", val_z )
+        #print( "x: ", val_x, "y: ", val_y, "z: ", val_z, "L2: ", adc_accum, "en: ", out_en )
+        #print( "x: ", val_x, "z: ", val_z, "L2: ", adc_accum, "en: ", out_en )
         print_counter = 0
 
 
