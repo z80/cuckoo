0811b7600fa1df72393b60825fbaff2dc948359d
me <me@me.me>
2026-08-01 15:18:00 -0700

    3 modes: normal, USB driver, and bootloader.

--- python/module_planning/boot.py
+++ python/module_planning/boot.py
@@ -2,9 +2,9 @@
 # Put app code in main.py
 
 import pyb
-from debug_mode import is_debug_mode
+from debug_mode import is_pin_down
 
-dbg = is_debug_mode()
+dbg = is_pin_down( pin_name='C7', hold_time_ms=500 )
 
 if dbg:
     # Act as a serial and a storage device
@@ -13,11 +13,16 @@ if dbg:
     pyb.main('main_debug.py')
 
 else:
-    # Only serial and disable REPL
-    pyb.repl_uart(None)
-    pyb.usb_mode('VCP')
-    # Execute a normal experimental main.
-    pyb.main('main.py')
+    firmware_upgrade = is_pin_down( pin_name='C8', hold_time_ms=500 )
+    if firmware_upgrade:
+        pyb.bootloader()
+
+    else:
+        # Only serial and disable REPL
+        pyb.repl_uart(None)
+        pyb.usb_mode('VCP')
+        # Execute a normal experimental main.
+        pyb.main('main.py')
 
 
 
